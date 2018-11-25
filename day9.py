4c8da912755fbcbcead33182f692fc700f10db54
Moon Munar <moonmunar@Moons-MBP.attlocal.net>
2018-11-24 23:06:06 -0600

    day9

---
+++ day9.py
@@ -0,0 +1,20 @@
+def sayhi(name, nationality):
+    print("Hello " + name + "you are " + nationality)
+
+
+sayhi("Sam ", "american")
+sayhi("Said ", "arab")
+
+
+def human(race, country):
+    print("He is " + race + "and he is from " + country)
+
+human("asian ", "China")
+human("european ", "Italy")
+
+
+def math(word1, word2):
+    print(word1 + " + " + word2 + " = Love")
+
+
+math("Anthony", "Alice")
